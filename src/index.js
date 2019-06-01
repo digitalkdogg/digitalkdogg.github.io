b29dd3dc0213828e1de928e99a8bc898cc6efd24
Kevin Bollman <digitalkdogg@gmail.com>
2019-06-01 16:50:42 -0500

    cleaning up react warnings and errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 import Body from './templates/Body';
 import '../public/includes/css/style.css';
 
-ReactDOM.render(
-  <Body />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <Body />
+   </BrowserRouter>
+ ), document.getElementById('root'));
