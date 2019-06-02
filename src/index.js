a1e4231bc246311ed35e2f3c5fca4c2008156c09
Kevin Bollman <digitalkdogg@gmail.com>
2019-06-02 03:46:39 -0500

    moved fav icon to the root

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 
 import Body from './templates/Body';
 import '../public/includes/css/style.css';
 
-ReactDOM.render((
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <Body />
-   </BrowserRouter>
- ), document.getElementById('root'));
+var e = React.createElement;
+ReactDOM.render(
+    e('div', null, <Body /> ),
+    document.getElementById('root')
+);
