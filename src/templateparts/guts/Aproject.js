8567f80ab2627d7bd2895cf38107ecf31c9fd05f
Kevin Bollman <kevin.bollman@walmart.com>
2019-06-14 19:17:27 -0500

    made modal basics happen on compiled version

--- src/templateparts/guts/Aproject.js
+++ src/templateparts/guts/Aproject.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 
 class Aproject extends Component {
 
-	constructor() {
+	constructor(props) {
       super();
-      this.state = { }
+      this.state = {
+        'props': props,
+        'modalclass': 'hidden' 
+      }
  }
+
+ exec_proj_click (project, that) {
+    if (project.props.project.type =='url') {
+      var url = project.props.project.href
+      var win = window.open(url, '_blank');
+      win.focus();
+    } else {
+      project.setState({'modalclass': 'visible'})
+    }
+  }
+
+  close_modal(project, that) {
+    project.setState({'modalclass': 'hidden'})
+  }
 
 
 	render(props){
 		 return (
+      <div id = "project-container">
         <div className="project">
           <div className="title">
             {this.props.project.title}
@@ -18,11 +36,28 @@ class Aproject extends Component {
             {this.props.project.desc}
           </div>
           <div className="btn-container">
-            <a href={this.props.project.href} target="_blank">
-              <button>View More</button>
+         
+            <a>
+              <button onClick = {this.exec_proj_click.bind(this.props.project, this)}>View More</button>
             </a>
+
           </div>
         </div>
+         <div id = "modal" className = {'modal ' + this.state.modalclass}>
+           <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" onClick = {this.close_modal.bind(this.props.project, this)} >&times;</button>
+                <h4 className="modal-title">{this.props.project.title}</h4>
+            </div>
+            <div className="modal-body">
+              <p>{this.props.project.desc}</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" onClick = {this.close_modal.bind(this.props.project, this)}>Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
       )
 	}
 }
--- src/templateparts/guts/Projects.js
+++ src/templateparts/guts/Projects.js
@@ -13,22 +13,28 @@ class Projects extends Component {
             'id': 1,
             'title': 'Millwood',
             'desc': 'This is a website I did for Millwood Church in rogers.  The site is built on wordpress and the features include a customized theme, an events calendar, and a blog news feed.  The updates of content is managed solely by the power users of the church.',
+            'type': 'url',
             'href': 'https://millwoodchurchnwa.com'
           }, 
           'checkbook' : {
             'id': 2,
             'title': 'Checkbook',
             'desc': 'This was a personal project that I did on my own.  It uses php code igniter along with mysql db server',
-            'href' : './src/includes/img/checkbook.png'
+            'type': 'modal',
+            'href' : './src/includes/img/checkbook.png',
+            'iclicked': function (datext) {console.log(datext)}
         }, 
           'resume' : {
             'id': 3,
             'title': 'Resume Builder',
             'desc': 'This was another personal project that I did on my own.  As I started my job search I wanted an easy way to manage different resume layouts.  This tool that I build allows me to do that.',
+            'type': 'modal',
             'href' : './src/includes/img/resume_admin.png'
         } } 
       }
  }
+
+
 
 
 	render(props){ 
@@ -40,7 +46,8 @@ class Projects extends Component {
 
             <Aproject project={this.state.projs.millwood} />
             <Aproject project={this.state.projs.checkbook} />
-            <Aproject project={this.state.projs.resume} />
+            <Aproject project={this.state.projs.resume} i />
+           
            </div>
       )
 	}
