17b8046ca78b31192126640e5cb19089f4530923
Kevin Bollman <digitalkdogg@gmail.com>
2020-01-18 14:37:26 -0600

    fixed glyphicons in compiled mode

--- src/templateparts/header/Aicon.js
+++ src/templateparts/header/Aicon.js
@@ -9,7 +9,7 @@ import React from 'react';
     render(props) {
       return(
          <div className={this.props.valign}>
-            <i className={this.props.class} aria-hidden="true"></i>
+            <i className={this.props.className} aria-hidden="true"></i>
           </div>
       )
     }
