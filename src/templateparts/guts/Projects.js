0b0d7852738e1c123cf37e024a2dce850bc1fcd9
Kevin Bollman <digitalkdogg@gmail.com>
2020-02-23 14:27:27 -0600

    clean up guts and projects

--- src/templateparts/guts/Projects.js
+++ src/templateparts/guts/Projects.js
@@ -5,9 +5,9 @@ import Quote from '../utils/Quote';
 import Aproject from '../guts/Aproject';
 import Swiper from 'react-id-swiper';
 import Utilsjs from '../utils/Utilsjs';
+import Projs from '../../JSON/Projects.json';
 
-import "../../includes/swiper/swiper.min.css";
-
+import '../../includes/swiper/swiper.min.css';
 
 class Projects extends Component {
 
@@ -15,84 +15,6 @@ class Projects extends Component {
       super();
       this.state = {
         'modalstate': 'hidden',
-        projs: {
-          'millwood' : {
-            'id': 1,
-            'title': 'Millwood',
-            'desc': 'When my church approach me i was super excited to do this project.  Not only did I get to do what I love but I felt I got to help my community as well.  We migrated the site from a old 5 year old drupal site.  The site is features a fully customized theme along with a facebook and constant contact integration. The website is now fully maintain by the staff at Millwood Christian Church',
-            'type': 'url',
-						'thumb': 'https://digitalkdogg.github.io/src/includes/img/millwood.png',
-            'thumbname' : 'millwood',
-						'platform': 'PHP / Wordpres',
-						'skills': 'PHP, Wordpres, jQuery, git, UX Design, CMS',
-            'href': 'https://millwoodchurchnwa.com'
-          },
-          'checkbook' : {
-            'id': 2,
-            'title': 'Checkbook',
-            'desc': 'Checkbook was a personal project that I did a couple of years ago that I still use today.  I needed an application that would allow me to keep track of my bills and help me balance my budget.  I didn\'t lke anything out there so I wrote my own.  The app is made in php 7 with codeigniter and runs off my own MySQL server',
-            'type': 'url',
-						'platform': 'PHP - Codeigniter',
-						'thumb': 'https://digitalkdogg.github.io/src/includes/img/checkbook_scaled.png',
-            'thumbname' : 'checkbook',
-						'skills': 'PHP, MySQL, Bootstrap 3, Responsive Design, Restfull API Design ',
-            'href' : '#'
-        },
-          'olympic' : {
-            'id': 3,
-            'title': 'Olympic Inspection',
-            'desc': 'Olympic Inspection is a site that I develop for a customer to help start up thier small business doing home inspections. It is really interesting to work with a company from the ground up and really see how the website really does help and grow their business.  The site is build with wordpress and features a custom theme along with a contact / db form.',
-            'type': 'url',
-						'thumb': 'https://digitalkdogg.github.io/src/includes/img/olympic_inspection.png',
-            'thumbname' : 'olympic',
-						'platform': 'PHP - Wordpress',
-						'skills': 'PHP, MySQL, jQuery',
-            'href' : 'https://olympicinspection.com'
-        },
-          'checkboxme' : {
-            'id': 4,
-            'title': 'Checkbox.me',
-            'desc': 'This was my first attempted at making a jQuery plugin.  I need this because I was working on a project that required me to make a little bit more fancy of a checkbox.  I did some digging around and came up wiht this way to render a checkbox using a div and an web font.  I decided to turn this into a plugin in case anyone wanted to use it.',
-            'type': 'url',
-						'thumb': 'https://digitalkdogg.github.io/src/includes/img/checkboxme.png',
-            'thumbname' : 'checkboxme',
-						'platform': 'jQuery',
-						'skills': 'jQuery, Plugin Design, javascript',
-            'href': 'https://digitalkdogg.github.io/checkbox.github.io'
-          },
-          'millwoodadmin' : {
-            'id': 5,
-            'title': 'Millwood Admin',
-            'desc': 'To help support the Millwood church website, we need an applicaiton to take care of some administrative tasks.  For example, the product owner will create a constant contact newsletter and than use this admin site to import into the news section of the website.  We did it this way instead of doing a straight api feed because we wanted the ability to pick and choose which news letters show up on the website.',
-						'type': 'url',
-						'thumb': 'https://digitalkdogg.github.io/src/includes/img/millwood_admin.png',
-            'thumbname' : 'millwoodadmin',
-						'platform': 'PHP 7',
-						'skills': 'PHP, javascript, Restful API, data integration',
-						'href': '#'
-          },
-          'plexmanager' : {
-            'id': 6,
-            'title': 'Plex Manager',
-            'desc': 'I have over 200 movies stored on my plex server.  I made this application to help me categorize and optimize my plex library.  The plex application doesn\'t have this functionality.  Built with Laravel 5 and leverages the plex api service.' ,
-            'type': 'url',
-            'thumb': 'https://digitalkdogg.github.io/src/includes/img/plex_admin.png',
-            'thumbname' : 'plexmanager',
-            'platform': 'PHP - Laravel',
-            'skills': 'PHP, javascript, Restful API',
-            'href': '#'
-          },
-          'resume' : {
-            'id': 7,
-            'title': 'Resume Builder',
-            'desc': 'Another personal project that I did in my spare time.  I wanted something to help me keep track of new opportunities that I am interested and I have applied for.  The application is build in code ignitor and uses the mPDF class to dymanically generate PDF\'s',
-            'type': 'url',
-            'thumb': 'https://digitalkdogg.github.io/src/includes/img/resume_admin.png',
-            'thumbname': 'resumeadmin',
-            'platform': 'PHP - Codeigniter',
-            'skills': 'PHP, MySQL, jQuery, boostrap',
-            'href' : '.#'
-        } }
       }
  }
 
@@ -102,8 +24,10 @@ componentDidMount() {
        document.querySelector('.swiper-pagination-total').innerText = document.querySelectorAll('#project-container').length;
       }
     },100);
-}
 
+ 
+ 
+}
 
   close_modal(project) {
 
@@ -140,7 +64,6 @@ componentDidMount() {
       breakpoints: {
         800: {
           slidesPerView: 2,
-      /*    slidesPerGroup: 1, */
           navigation: {
               nextEl: '.swiper-button-next',
               prevEl: '.swiper-button-prev'
@@ -163,13 +86,13 @@ componentDidMount() {
                 <P class="quote-name" text=" -Thomas Edison" />
                 <P text="" />
                 <Swiper {...params}>
-                  <Aproject project={this.state.projs.millwood} />
-                  <Aproject project={this.state.projs.checkbook} />
-                  <Aproject project={this.state.projs.olympic}/>
-                  <Aproject project={this.state.projs.checkboxme}/>
-                  <Aproject project={this.state.projs.millwoodadmin} />
-                  <Aproject project={this.state.projs.plexmanager} />
-                  <Aproject project={this.state.projs.resume}/>
+                  <Aproject project={Projs.millwood} />
+                  <Aproject project={Projs.checkbook} />
+                  <Aproject project={Projs.olympic}/>
+                  <Aproject project={Projs.checkboxme}/>
+                  <Aproject project={Projs.millwoodadmin} />
+                  <Aproject project={Projs.plexmanager} />
+                  <Aproject project={Projs.resume}/>
 
 
 
--- src/templates/Guts.js
+++ src/templates/Guts.js
@@ -16,42 +16,6 @@ class Guts extends Component {
         projects: 'My Projects',
         skills  : 'Skills',
         contact: 'Contact Me',
-        myskills : {
-          'frontend': {
-            'id': 1,
-            'text': 'Front End',
-            'data': {
-              'html': {
-                'id': 1,
-                'text':'HTML5'
-              }, 
-              'css': {
-                'id': 2,
-                'text':'CSS3'
-              }
-            }
-          },
-          'backend': {}
-        },
-        projs: {
-          'millwood' : {
-            'id': 1,
-            'title': 'Millwood',
-            'desc': 'This is a website I did for Millwood Church in rogers.  The site is built on wordpress and the features include a customized theme, an events calendar, and a blog news feed.  The updates of content is managed solely by the power users of the church.',
-            'href': 'https://millwoodchurchnwa.com'
-          }, 
-          'checkbook' : {
-            'id': 2,
-            'title': 'Checkbook',
-            'desc': 'This was a personal project that I did on my own.  It uses php code igniter along with mysql db server',
-            'href' : './public/includes/img/checkbook.png'
-        }, 
-          'resume' : {
-            'id': 3,
-            'title': 'Resume Builder',
-            'desc': 'This was another personal project that I did on my own.  As I started my job search I wanted an easy way to manage different resume layouts.  This tool that I build allows me to do that.',
-            'href' : './public/includes/img/resume_admin.png'
-        } }
       }
     }
 
