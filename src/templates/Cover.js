653bbad9eac0e56de0c43aad05286d603792c0d9
Kevin Bollman <digitalkdogg@gmail.com>
2020-03-01 18:15:39 -0600

    moved banner i css to its own file and made box on home happen

--- src/templates/Cover.js
+++ src/templates/Cover.js
@@ -11,7 +11,18 @@ class Cover extends Component {
 
     render() {
       return(
-        <div id = "cover">Hello I am Kevin Bollman<br />A Full Stack Developer</div>
+        <div id = "cover">
+          <div className = "box">
+            <p className="tag"> Hello I am Kevin Bollman</p>
+             <hr />
+            <p className = "sub">I build cool things and I love what I do</p>
+
+            <p>Since I began my web design journey over 10 years, I have help many people / organization with their web design needs.  
+            I enjoy helping people with connect with their technology.  I am innovative, forward thinking and I am passionate about the
+            work I do.  I challenge my self to learn new technologies to advance my skills and provide value to what ever team I am on.
+            </p>
+          </div>
+        </div>
       )
   }
 }
