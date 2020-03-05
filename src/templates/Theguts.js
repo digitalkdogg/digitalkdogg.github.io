208f9e7ea840a1aef0ba8c69a0e42063f6bdab67
Kevin Bollman <digitalkdogg@gmail.com>
2020-03-05 15:34:47 -0600

    removed extra space in theguts

--- src/templates/Theguts.js
+++ src/templates/Theguts.js
@@ -32,7 +32,6 @@ class Theguts extends Component {
           				<P class="tag" text="Hello I am Kevin Bollman" />
              			<hr />
             			<P class="sub" text="I build cool things and make them work" />
-
             			<P text="Since I began my web design journey over 10 years, I have help many people / organization with their web design needs.
             				I enjoy helping people with connect with their technology.  I am innovative, forward thinking and I am passionate about the
             				work I do.  I challenge my self to learn new technologies to advance my skills and provide value to what ever team I am on." />
