8c01135129e46c2d2cc92e54203afb97508f079f
Kevin Bollman <kevin.bollman@walmart.com>
2020-03-06 14:11:08 -0600

    start of projects

--- src/JSON/Projects.js
+++ src/JSON/Projects.js
@@ -9,7 +9,37 @@ class Projects extends Component {
 	render(props){
 		 return (
      <div id="projects" className="section hidden">
-       The Projects section
+		 <div className="row-wrap skills-wrap">
+			 <div className="block c5 box-shadow box">
+				 <div className="title">Millwood Church</div>
+
+			 </div>
+			 <div className="block c5 box-shadow box">
+				 <div className="title">Olympic Inspection</div>
+
+			 </div>
+		 </div>
+
+		 <div className="row-wrap skills-wrap">
+			 <div className="block c5 box-shadow box">
+				 <div className="title">Checkbook</div>
+
+			 </div>
+			 <div className="block c5 box-shadow box">
+				 <div className="title">Checkbox.js</div>
+
+			 </div>
+		 </div>
+		 <div className="row-wrap skills-wrap">
+			<div className="block c5 box-shadow box">
+				<div className="title">Checkbook</div>
+
+			</div>
+			<div className="block c5 box-shadow box">
+				<div className="title">Checkbox.js</div>
+
+			</div>
+		</div>
      </div>
       )
 	}
