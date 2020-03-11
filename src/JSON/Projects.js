1aa5c6d684fd30f0e732eed21c5bd46988fb118c
Kevin Bollman <digitalkdogg@gmail.com>
2020-03-10 19:23:20 -0500

    clean up of projects flip action

--- src/JSON/Projects.js
+++ src/JSON/Projects.js
@@ -17,14 +17,30 @@ class Projects extends Component {
 
 						 <div class="flip-card-inner">
 
-							 <div class="flip-card-front" style = {millwoodstyle}>
-								
+							 <div class="flip-card-front" style = {millwoodstyle}> 
+							 	<div class="flip-card-cover"></div>
+							 	<div class="flip-card-title">Millwood Christian Church</div>
 							 </div>
 
 							 <div class="flip-card-back">
 								 <h1 className="title">Millwood Christian Church</h1>
-								 <p>cool description here</p>
-								 <p>also here</p>
+								 <div class = "row">
+								 	<div className = "name col align-left">Project Brief : </div> 
+								 	<div className = "value col">I did this project for my church to give back to my community.  We migrated the domain from existing drupal site and manage the new site from wire frame to completion</div>
+								 </div>
+								  <div class = "row">
+								 	<div className = "name col align-left">Features : </div> 
+								 	<div className = "value col">
+								 		<li className = "nobullet">Customized Theme</li>
+								 		<li className = "nobullet">Dynamic Restful API Feed</li>
+								 		<li className = "nobullet">Online Donation Portal</li>
+								 	</div>
+								 </div>
+								 <div class = "row">
+								 	<div className = "name col align-left">Technologies : </div> 
+								 	<div className = "value col">PHP, UX Design, Word Press, CSS, REST API</div>
+								 </div>
+								 
 							 </div>
 
 						 </div>
