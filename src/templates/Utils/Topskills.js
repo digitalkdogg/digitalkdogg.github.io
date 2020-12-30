bf0a666edc90ac218717f2c149f52a8fbc44463e
Kevin Bollman <digitalkdogg@gmail.com>
2020-12-30 14:27:40 -0600

    change opacity on tops skills bg

--- src/templates/Utils/Topskills.js
+++ src/templates/Utils/Topskills.js
@@ -17,30 +17,34 @@ class Topskills extends Component {
             <hr />
             <div id="top-skills-wrap" className="flex max-width-1200">
                 <div className="icon-wrap">
-                    <i className="fas fa-terminal" aria-hidden="true"></i>
-                    <i className="fas fa-keyboard" aria-hidden="true"></i>
-                    <i className="fab fa-css3" aria-hidden="true"></i>
-                    <i className="fas fa-laptop-code" aria-hidden="true"></i>
-                    <i className="fab fa-node" aria-hidden="true"></i>
-                    <i className="fas fa-coffee" aria-hidden="true"></i>
-                    <i className="fab fa-connectdevelop" aria-hidden="true"></i>
-                    <br />
-                    <i className="fas fa-server" aria-hidden="true"></i>
-                    <i className="fab fa-jsfiddle" aria-hidden="true"></i>
-                    <i className="fas fa-database" aria-hidden="true"></i>
-                    <i className="fas fa-table" aria-hidden="true"></i>
-                    <i className="fas fa-hourglass-start" aria-hidden="true"></i>
-                    <i className="fas fa-ruler-combined" aria-hidden="true"></i>
-                    <br />
-                    <i className="fas fa-globe-americas" aria-hidden="true"></i>
-                    <i className="fab fa-react" aria-hidden="true"></i>
-                    <i className="fab fa-wordpress" aria-hidden="true"></i>
-                    <i className="fab fa-angular" aria-hidden="true"></i>
-                    <br />
-                    <i className="fab fa-dev" aria-hidden="true"></i>
-                    <i className="fab fa-js" aria-hidden="true"></i>
-                    <i className="fab fa-chrome" aria-hidden="true"></i>
-                
+                    <div class = "row">
+                        <i className="fas fa-terminal" aria-hidden="true"></i>
+                        <i className="fas fa-keyboard" aria-hidden="true"></i>
+                        <i className="fab fa-css3" aria-hidden="true"></i>
+                        <i className="fas fa-laptop-code" aria-hidden="true"></i>
+                        <i className="fab fa-js" aria-hidden="true"></i>
+                        <i className="fas fa-coffee" aria-hidden="true"></i>
+                        <i className="fab fa-jsfiddle" aria-hidden="true"></i>
+                    </div>
+                    <div class = "row">
+                        <i className="fas fa-server" aria-hidden="true"></i>
+                        <i className="fab fa-connectdevelop" aria-hidden="true"></i>
+                        <i className="fas fa-database" aria-hidden="true"></i>
+                        <i className="fas fa-table" aria-hidden="true"></i>
+                        <i className="fas fa-hourglass-start" aria-hidden="true"></i>
+                        <i className="fas fa-ruler-combined" aria-hidden="true"></i>
+                    </div>
+                    <div class = "row">
+                        <i className="fas fa-globe-americas" aria-hidden="true"></i>
+                        <i className="fab fa-react" aria-hidden="true"></i>
+                        <i className="fab fa-wordpress" aria-hidden="true"></i>
+                        <i className="fab fa-angular" aria-hidden="true"></i>
+                    </div>
+                    <div class = "row">
+                        <i className="fab fa-dev" aria-hidden="true"></i>
+                        <i className="fab fa-node" aria-hidden="true"></i>
+                        <i className="fab fa-chrome" aria-hidden="true"></i>
+                    </div>
       
                 </div>
                 <div className="details-wrap">
