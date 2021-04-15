7ec69465e0b40cebdb6e6f34679206f3773882ae
Kevin Bollman <digitalkdogg@gmail.com>
2021-04-15 10:18:26 -0500

    Update project to include walmart world

--- src/JSON/Projects.js
+++ src/JSON/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import '../includes/css/projects.css';
 import millwood from '../includes/img/millwood_thumb.jpg';
+import walmartworld from '../includes/img/walmartworld.png';
 import olympic from '../includes/img/olympic_inspection.png';
 import checkbook from '../includes/img/checkbook_scaled.png';
 import plex from '../includes/img/plex_admin.png';
@@ -14,12 +15,12 @@ class Projects extends Component {
 render(){
 return (
      <div id="projects" className="section">
-		
+
 
 		<div className="mobile-space-small desktop-space-large"></div>
 
-		<Aproject 
-			title='Millwood Christian Church' 
+		<Aproject
+			title='Millwood Christian Church'
 			href='https://millwoodchurchnwa.com/'
 			desc='A website I desing and maintain for my church. I enjoy giving back to my community'
 			tags={[
@@ -36,12 +37,33 @@ return (
 			imgsrc={millwood}
 		/>
 
-	
+
 		<div className="mobile-space-small desktop-space-large"></div>
 		<div className="mobile-space-small desktop-space-large"></div>
 
-			<Aproject 
-			title='Olympic Home Inspections' 
+    <Aproject
+      title='Walmart World'
+      href='https://walmartworld.com'
+      desc='This was my first public site I did for Walmart since most of my work is done for the intranet'
+      tags={[
+        {name:'Adobe AEM', value:'fas fa-braille'},
+        {name:'UX Design', value:'fas fa-drafting-compass'},
+        {name:'REST API', value:'fas fa-stream'}]}
+      projects={[
+        {'name':'Material Design'},
+        {'name':'Restfull API (See milestone section)'},
+        {'name':'CMS Customization'}
+      ]}
+      imgalt='Walmart World'
+      imgsrc={walmartworld}
+    />
+
+
+    <div className="mobile-space-small desktop-space-large"></div>
+    <div className="mobile-space-small desktop-space-large"></div>
+
+			<Aproject
+			title='Olympic Home Inspections'
 			href='http://olympicinspection.com'
 			desc='I did this project for a small business to help get their business off the ground.  I work directly with the customer and help purchase a domain, reviewed mockups and developed the site in support of their business.'
 			tags={[
@@ -54,13 +76,13 @@ return (
 				{name:'Custom Contact Form'}]}
 			imgalt='Olympic Inspection'
 			imgsrc={olympic}
-		/>	
-	
+		/>
+
 		<div className="mobile-space-small desktop-space-large"></div>
 		<div className="mobile-space-small desktop-space-large"></div>
-		
-		<Aproject 
-			title='Checkbook' 
+
+		<Aproject
+			title='Checkbook'
 			desc='This was a personal project that I did in php codeigniter.  I needed a way to keep track of my bills and I was not too happy with any of the apps out there so I made my own.'
 			tags={[
 				{name:'PHP', value:'fab fa-php'},
@@ -72,14 +94,14 @@ return (
 				{name:'Data Dashboard'}]}
 			imgalt='Checkbook'
 			imgsrc={checkbook}
-		/>	
-		
-		
+		/>
+
+
 		<div className="mobile-space-small desktop-space-large"></div>
 		<div className="mobile-space-small desktop-space-large"></div>
-		
-		<Aproject 
-			title='Plex Movie Manager' 
+
+		<Aproject
+			title='Plex Movie Manager'
 			desc='Another personal project that I did.  The goal was to optimize my plex movie library.  I wrote this in laravel and it uses the plex api to tab into my library and allow me to see the details of eash of my movies'
 			tags={[
 				{name:'PHP 7', value:'fab fa-php'},
@@ -90,8 +112,8 @@ return (
 				{name:'Reporting Dashboard'}]}
 			imgalt='Plex Movie Manager'
 			imgsrc={plex}
-		/>	
-		
+		/>
+
 		<div className="mobile-space-small desktop-space-large"></div>
 		<div className="mobile-space-small desktop-space-large"></div>
 	</div>
